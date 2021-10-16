cea8867a66f69aef042c72fcd7435f0a87c8e9e4
Joshua Barrett <jjbarr.public@gmail.com>
2021-10-16 14:31:33 -0400

    used tick not profile, noted dtrace host-side doesn't work yet.

--- cputime_notlinux.d
+++ cputime_notlinux.d
@@ -1,4 +1,5 @@
 #!/usr/sbin/dtrace -s
 #pragma D option quiet
+/*this doesn't actually necessarily work yet...*/
 profile:::profile-400hz /pid/ {@run[cpu]=count();}
-profile:::profile-4hz {printa("%@8u ", @run); printf("\n"); clear(@run)}
+profile:::tick-4hz {printa("%@8u ", @run); printf("\n"); clear(@run)}
